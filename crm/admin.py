435cae17154a7d88a1f170757227c6052d04fd46
Vladislav Radchenko <specforworkwithpeop@gmail.com>
2021-07-23 18:00:38 +0300

    Add models

--- crm/admin.py
+++ crm/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Client, Employee, Application
+
+admin.site.register(Client)
+admin.site.register(Employee)
+admin.site.register(Application)
--- crm/models.py
+++ crm/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
-# Create your models here.
+SPEC = [
+    ('consultant', 'Consultant'),
+    ('master', 'Master'),
+    ('service_staff', 'Service staff')
+]
+
+TYPE = [
+    ('repair', 'Repair'),
+    ('service', 'Service'),
+    ('consultation', 'Consultation')
+]
+
+STATUS = [
+    ('open', 'Open'),
+    ('in_work', 'In work'),
+    ('closed', 'Closed')
+]
+
+class Client(models.Model):
+
+    first_name = models.TextField(max_length=20)
+    second_name = models.TextField(max_length=50)
+    middle_name = models.TextField(max_length=20)
+    email = models.EmailField()
+    tg_nick = models.TextField(null=True, blank=True) #Optional
+
+    def __str__(self):
+        return f'{self.first_name} {self.middle_name} {self.second_name}'
+
+class Employee(models.Model):
+
+    first_name = models.TextField(max_length=20)
+    second_name = models.TextField(max_length=50)
+    middle_name = models.TextField(max_length=20)
+    speciality = models.CharField(choices=SPEC, max_length=13)
+    email = models.EmailField()
+    tg_nick = models.TextField(null=True, blank=True) #Optional
+
+    def __str__(self):
+        return f'{self.second_name}: {self.speciality}'
+    
+class Application(models.Model):
+
+    number = models.TextField(max_length=20)
+    client = models.ForeignKey(Client, on_delete=CASCADE)
+    employee = models.ForeignKey(Employee, on_delete=CASCADE, null=True, blank=True)
+    apl_type = models.CharField(choices=TYPE, max_length=12)
+    status = models.CharField(choices=STATUS, max_length=7)
+    start_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField(help_text="Deadline")
+
+    def __str__(self):
+        return self.number
